ff1f167a56540eaeadc31957db63bd4d5228cdfa
Your Name <you@example.com>
2020-05-05 11:59:53 +0800

    first commit

---
+++ averagen.py
@@ -0,0 +1,12 @@
+N = 10
+sum = 0
+count = 0
+print("pkease input 10 number:")
+while count < N:
+    number = float(input())
+    sum = sum + number
+    count =count + 1
+average = sum / N
+
+print("N = {}, Sum = {}".format(N,sum))
+print("Average = {:.2f}".format(average))
